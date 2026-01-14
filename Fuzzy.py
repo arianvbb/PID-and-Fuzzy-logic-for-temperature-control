68688423051b7372745f3cf131af67f31d849446
Arian Harveland <130916492+arianvbb@users.noreply.github.com>
2026-01-14 22:05:09 +0100

    Update Fuzzy.py

--- Fuzzy.py
+++ Fuzzy.py
@@ -1,5 +1,9 @@
 from main import Furnace
 import math
+import matplotlib.pyplot as plt
+
+Temperatures = []
+Time = []
 
 class Fuzzy:
     def __init__(self, furnace, ideal_temperature = 21):
@@ -22,7 +26,7 @@ class Fuzzy:
             return 0
         elif difference > 2 and difference < 4:
             return (difference - 2) / 2
-        elif self.difference >= 4 and difference < 6:
+        elif difference >= 4 and difference < 6:
             return (6 - difference) / 2
         elif difference <= 2:
             return 0
@@ -39,31 +43,47 @@ class Fuzzy:
         else:
             print("Error")
 
-    # The actual Fuzzy theory, we have the different values of the gaps and simply adding the numbers with their selective output and dividing by the denominator which is the 3 numbers summed (without the output)
+    def no_gap(self, difference):
+        if difference <= 0.5:
+            return 1
+        if difference < 1:
+            return (1 - difference) / 0.5
+        else:
+            return 0
+
+    # The actual Fuzzy theory, we have the different values of the gaps and simply adding the numbers with their selective output and dividing by the denominator which is the 4 numbers summed (without the output)
     def process(self):
         self.difference = self.ideal_temperature - self.furnace.temperature
 
+        none = self.no_gap(self.difference)
         small = self.small_gap(self.difference)
         medium = self.medium_gap(self.difference)
         large = self.large_gap(self.difference)
 
+        # Playing around with these values may change the results drastically.
+        No_Output = 0.30
         Low_Output = 0.40
-        Medium_Output = 0.50
+        Medium_Output = 0.60
         High_Output = 0.90
 
-        denom = small + medium + large
+        denom = none + small + medium + large
 
-        power = ((small * Low_Output) + (medium * Medium_Output) + (large * High_Output)) / denom if denom > 0 else 0
+        power = ((none * No_Output) + (small * Low_Output) + (medium * Medium_Output) + (large * High_Output)) / denom if denom > 0 else 0
 
         self.furnace.step(power)
 
 fuzzy = Fuzzy(Furnace(), ideal_temperature = 21)
 
-for t in range (2000):
+for t in range (100):
     fuzzy.furnace.ambient = 12 + 4.0 * math.sin(2* math.pi * t / 24) # Makes a sin-wave out of the ambience temperature so it isn't constant. This program counts 1 rotation as an hour therefor this sin-wave accounts for night and day rotation.
     fuzzy.process()
+    Time.append(t)
+    Temperatures.append(fuzzy.furnace.temperature)
     print(f"Step {t}: {fuzzy.furnace.temperature:.2f}°C") #Prints the temperature with 2 decimals every hour.
 
-
-        
-    
+# Plots the collected data.
+plt.plot(Time, Temperatures)
+plt.xlabel("Time (h)")
+plt.ylabel("Temperatures (C)")
+plt.axhline(21, color = 'red', label = "Ideal_temp")
+plt.show()    
